2bcf8ecba175aa55dcd4bc202b0375c4fe3a2635
தகவலுழவன் <tha.uzhavan@gmail.com>
2018-05-14 12:58:54 +0530

    Update and rename finding-duplicates.py to finding-duplicate-lines-of-csv.py
    ஒரு csvகோப்பில்  உள்ள வரிகளில் ஒன்றுபோல, மற்றொன்று இருப்பின் களைந்து புதிய கோப்பாகத் தரும்.

--- finding-duplicate-lines-of-csv.py
+++ finding-duplicate-lines-of-csv.py
@@ -1,10 +1,10 @@
-count_lines = sum(1 for line in open('contributors-cleaned.csv'))
+count_lines = sum(1 for line in open('cleaned.csv'))
 integer2string = str(count_lines)
 print('Total contributors = '+integer2string)
 
-content = open('uncleaned-contributors.csv','r').readlines()
+content = open('uncleaned.csv','r').readlines()
 content4set = set(content)
-cleanedcontent = open('contributors-cleaned.csv','w')
+cleanedcontent = open('cleaned.csv','w')
 for i, line in enumerate(content4set):
 	cleanedcontent.write("{}.{}".format(str(i+1),line.replace('பக்கம்','அட்டவணை_பேச்சு')))
 	line=line.strip()
