b325b661789246996955d5f2fa560274b4849d0c
தகவலுழவன் <tha.uzhavan@gmail.com>
2019-06-15 11:54:47 +0530

    Create ws-ta-heder-middle-footer-separater.py
    விக்கிமூல நூற் பக்கமொன்றினை தனித்தனியே, மூன்றுபகுதிகளாகக் காண்பதே முதற்படி ஆகும்.

---
+++ ws-ta-heder-middle-footer-separater.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+import csv, time, subprocess, re, pywikibot
+
+WAIT_TIME = 15
+with open('000-ws-ta-pre-clean.csv', 'r') as csvfile:
+	reader = csv.reader(csvfile,delimiter="~")
+#	count = 0
+	for row in reader:
+		subprocess.call("sed -i 1d 000-ws-ta-pre-clean.csv",shell=True)          
+	#if len(row) == 8: உயிரியல்
+	# if not 'booktitle' in row:   
+		wikiPage = row[0]#.decode('utf-8')
+		print (wikiPage)
+		
+		site1 = pywikibot.Site('ta', 'wikisource')
+		page = pywikibot.Page(site1, wikiPage)
+		existingText = page.text
+#		print('-------------------------------------------       தற்போதுள்ள பக்க உள்ளடக்கம்       ---------------------------------------------')
+#		print(type (existingText))
+#		print('\n\n' + existingText)
+#		print('\n\n' + '------------------------------        தற்போதுள்ள பக்க உள்ளடக்கம்   முடிவடைந்தது.       --------------------------------------------')
+		search_pattern = re.compile("\<noinclude\>.*?\<\/noinclude>", re.DOTALL)
+		tags = re.findall(search_pattern, existingText)
+		
+		headerTag1 = tags[0]
+		footerTag = tags[1]
+		no_tag_text = re.sub(search_pattern, '', existingText).strip()
+    		print('\n\n' + headerTag1)
